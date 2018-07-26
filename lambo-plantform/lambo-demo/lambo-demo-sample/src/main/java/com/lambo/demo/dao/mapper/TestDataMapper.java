9323a513331e0a7783f121aab0b566442f7288dc
sunzhen086 <stomz@163.com>
2018-07-26 21:49:18 +0800

    增加示例程序

---
+++ lambo-plantform/lambo-demo/lambo-demo-sample/src/main/java/com/lambo/demo/dao/mapper/TestDataMapper.java
@@ -0,0 +1,15 @@
+package com.lambo.demo.dao.mapper;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @ClassName TestDataMapper
+ * @Descirption 示例
+ * @Author sunzhen
+ * @Date 2018/7/26 21:21
+ **/
+public interface TestDataMapper {
+
+    List<Map> select();
+}
